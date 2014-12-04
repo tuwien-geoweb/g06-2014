dff6c722ceb21f8bb591e92799cd8694910968e0
ClaudiL <cloidolt@gmx.at>
2014-12-04 10:51:12 +0100

    Create censusmap.js

---
+++ block6/censusmap.js
@@ -0,0 +1,22 @@
+// Base map
+var osmLayer = new ol.layer.Tile({source: new ol.source.OSM()});
+
+// Census map layer
+var wmsLayer = new ol.layer.Image({
+  source: new ol.source.ImageWMS({
+    url: 'http://student.ifip.tuwien.ac.at/geoserver/wms',
+    params: {'LAYERS': 'g06_2014:normalized'}
+  }),
+  opacity: 0.6
+});
+
+// Map object
+olMap = new ol.Map({
+  target: 'map',
+  renderer: 'canvas',
+  layers: [osmLayer, wmsLayer],
+  view: new ol.View({
+    center: [-10764594.0, 4523072.0],
+    zoom: 5
+  })
+});
